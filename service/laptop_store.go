52508dc4831e4c5108bece939b9868e600cd8f5b
Tirouzh <mohammad.kolagar@gmail.com>
2022-07-03 22:44:10 +0430

    [FEATURE]: Implement the server's unary RPC handler

---
+++ service/laptop_store.go
@@ -0,0 +1,61 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/ctirouzh/gRPC/pb"
+	"github.com/jinzhu/copier"
+)
+
+// LaptopStore is an interface to store laptop
+type LaptopStore interface {
+	// Save saves the laptop to the store
+	Save(laptop *pb.Laptop) error
+}
+
+// InMemoryLaptopStore stores laptop in memory
+type InMemoryLaptopStore struct {
+	mutex sync.RWMutex
+	data  map[string]*pb.Laptop
+}
+
+// NewInMemoryLaptopStore returns a new InMemoryLaptopStore
+func NewInMemoryLaptopStore() *InMemoryLaptopStore {
+	return &InMemoryLaptopStore{
+		data: make(map[string]*pb.Laptop),
+	}
+}
+
+// ErrAlreadyExists is returned when a record with the same ID already exists in the store
+var ErrAlreadyExists = errors.New("record already exists")
+
+// Save saves the laptop to the store
+func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if store.data[laptop.Id] != nil {
+		return ErrAlreadyExists
+	}
+
+	other, err := deepCopy(laptop)
+	if err != nil {
+		return err
+	}
+
+	store.data[other.Id] = other
+	return nil
+}
+
+func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
+	other := &pb.Laptop{}
+
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return nil, fmt.Errorf("cannot copy laptop data: %w", err)
+	}
+
+	return other, nil
+}
