5cd11e75188c9d42d00c01157bfdcdf730e33f5a
Tirouzh <mohammad.kolagar@gmail.com>
2022-07-04 22:36:26 +0430

    [FEATURE]: Write the main server and client

--- Makefile
+++ Makefile
@@ -5,9 +5,6 @@ gen:
 	protoc --proto_path=proto proto/*.proto \
 	--go_out=:pb --go-grpc_out=:pb
 
-run: 
-	go run main.go
-
 server: 
 	go run cmd/server/main.go -port 8080	
 
---
+++ cmd/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/ctirouzh/gRPC/pb"
+	"github.com/ctirouzh/gRPC/service"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := flag.Int("port", 0, "the server port")
+	flag.Parse()
+	log.Printf("start server on port %d", *port)
+
+	laptopStore := service.NewInMemoryLaptopStore()
+	laptopServer := service.NewLaptopServer(laptopStore)
+	serverOptions := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(serverOptions...)
+
+	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+
+	address := fmt.Sprintf("0.0.0.0:%d", *port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+}
