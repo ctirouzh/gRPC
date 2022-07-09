38323b579835ded2ceb63f1ad56d7bc10913314f
Tirouzh <mohammad.kolagar@gmail.com>
2022-07-09 22:53:51 +0430

    [FEATURE]: Add server-side unary and stream interceptors

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -11,6 +12,26 @@ import (
 	"google.golang.org/grpc"
 )
 
+func unaryInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	log.Println("--> unary interceptor: ", info.FullMethod)
+	return handler(ctx, req)
+}
+
+func streamInterceptor(
+	srv interface{},
+	stream grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler,
+) error {
+	log.Println("--> stream interceptor: ", info.FullMethod)
+	return handler(srv, stream)
+}
+
 func main() {
 	port := flag.Int("port", 0, "the server port")
 	flag.Parse()
@@ -20,8 +41,11 @@ func main() {
 	imageStore := service.NewDiskImageStore("img")
 	ratingStore := service.NewInMemoryRatingStore()
 	laptopServer := service.NewLaptopServer(laptopStore, imageStore, ratingStore)
-	serverOptions := []grpc.ServerOption{}
-	grpcServer := grpc.NewServer(serverOptions...)
+	// serverOptions := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(unaryInterceptor),
+		grpc.StreamInterceptor(streamInterceptor),
+	)
 
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
 
