55b3085ecf09335890d99d427848686dd16ddd59
Tirouzh <mohammad.kolagar@gmail.com>
2022-07-10 02:51:19 +0430

    [FEATURE]: Attach token to the client's request context

--- client/auth_interceptor.go
+++ client/auth_interceptor.go
@@ -1,8 +1,12 @@
 package client
 
 import (
+	"context"
 	"log"
 	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 type AuthInterceptor struct {
@@ -27,6 +31,48 @@ func NewAuthInterceptor(
 	}
 
 	return interceptor, nil
+}
+
+func (interceptor *AuthInterceptor) Unary() grpc.UnaryClientInterceptor {
+	return func(
+		ctx context.Context,
+		method string,
+		req, reply interface{},
+		cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
+		log.Printf("--> unary interceptor: %s", method)
+
+		if interceptor.authMethods[method] {
+			return invoker(interceptor.attachToken(ctx), method, req, reply, cc, opts...)
+		}
+
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+}
+
+func (interceptor *AuthInterceptor) Stream() grpc.StreamClientInterceptor {
+	return func(
+		ctx context.Context,
+		desc *grpc.StreamDesc,
+		cc *grpc.ClientConn,
+		method string,
+		streamer grpc.Streamer,
+		opts ...grpc.CallOption,
+	) (grpc.ClientStream, error) {
+		log.Printf("--> stream interceptor: %s", method)
+
+		if interceptor.authMethods[method] {
+			return streamer(interceptor.attachToken(ctx), desc, cc, method, opts...)
+		}
+
+		return streamer(ctx, desc, cc, method, opts...)
+	}
+}
+
+func (interceptor *AuthInterceptor) attachToken(ctx context.Context) context.Context {
+	return metadata.AppendToOutgoingContext(ctx, "authorization", interceptor.accessToken)
 }
 
 func (interceptor *AuthInterceptor) scheduleRefreshToken(refreshDuration time.Duration) error {
