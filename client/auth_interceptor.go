1d45a29cbd5a402e50e4b78a39b86443fc0ff50f
Tirouzh <mohammad.kolagar@gmail.com>
2022-07-10 02:44:27 +0430

    [FEATURE]: Implement client interceptor, and Setup schedule to refresh token

---
+++ client/auth_interceptor.go
@@ -0,0 +1,67 @@
+package client
+
+import (
+	"log"
+	"time"
+)
+
+type AuthInterceptor struct {
+	authClient  *AuthClient
+	authMethods map[string]bool
+	accessToken string
+}
+
+func NewAuthInterceptor(
+	authClient *AuthClient,
+	authMethods map[string]bool,
+	refreshDuration time.Duration,
+) (*AuthInterceptor, error) {
+	interceptor := &AuthInterceptor{
+		authClient:  authClient,
+		authMethods: authMethods,
+	}
+
+	err := interceptor.scheduleRefreshToken(refreshDuration)
+	if err != nil {
+		return nil, err
+	}
+
+	return interceptor, nil
+}
+
+func (interceptor *AuthInterceptor) scheduleRefreshToken(refreshDuration time.Duration) error {
+	err := interceptor.refreshToken()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		// wait: how much time we need to wait before refreshing the token.
+		wait := refreshDuration
+		for {
+			time.Sleep(wait)
+			err := interceptor.refreshToken()
+			if err != nil {
+				// If an error occurs, we should only wait a short period of
+				// time, let’s say 1 second, before retrying it.
+				wait = time.Second
+			} else {
+				wait = refreshDuration
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (interceptor *AuthInterceptor) refreshToken() error {
+	accessToken, err := interceptor.authClient.Login()
+	if err != nil {
+		return err
+	}
+
+	interceptor.accessToken = accessToken
+	log.Printf("token refreshed: %v", accessToken)
+
+	return nil
+}
